270059ee2218e45612df93de80a9b1b9b16da574
arunviswam88 <arunviswam88@gmail.com>
2024-06-03 23:26:38 +0530

    adding header nav and banner wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () =>  import ('../components/login/login.component').then(login => login.LoginComponent)     
+        loadComponent: () =>  import ('../app/components/login/login.component').then(login => login.LoginComponent)     
     },
     {
         path: 'browse',
-        loadComponent: () =>  import ('../components/browse/browse.component').then(browse => browse.BrowseComponent)     
+        loadComponent: () =>  import ('../app/components/browse/browse.component').then(browse => browse.BrowseComponent)     
     }
 ];
